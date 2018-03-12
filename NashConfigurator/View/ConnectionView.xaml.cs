52fb0071f51bde19f4dc6c1f0ccf6dc0dd095dd7
Marvin Countryman <mcountryman@HBDOMAIN.local>
2018-03-12 17:11:26 -0400

    Initial Commit

---
+++ NashConfigurator/View/ConnectionView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NashConfigurator.ViewModel;
+
+namespace NashConfigurator.View
+{
+    /// <summary>
+    /// Interaction logic for ConnectionView.xaml
+    /// </summary>
+    public partial class ConnectionView : UserControl
+    {
+        public ConnectionView()
+        {
+            InitializeComponent();
+
+            DataContext = new ConnectionViewModel();
+        }
+    }
+}
---
+++ NashConfigurator/ViewModel/ConnectionViewModel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+using NashConfigurator.Model;
+
+namespace NashConfigurator.ViewModel
+{
+    public class ConnectionViewModel : INotifyPropertyChanged
+    {
+        private string hostname = "";
+        private string database = "";
+        private string username = "";
+        private string password = "";
+        private ISqlAuthenticator authenticator;
+        private bool canEditCredentials = true;
+        private List<string> hostnames = new List<string>();
+        private List<string> databases = new List<string>();
+        private ObservableCollection<ISqlAuthenticator> authenticators = new ObservableCollection<ISqlAuthenticator>() {
+            new WindowsAuthenticator()
+        };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Hostname {
+            get => hostname;
+            set {
+                hostname = value;
+                RaiseNotifyPropertyChanged("Hostname");
+            }
+        }
+        public string Database {
+            get => database;
+            set {
+                database = value;
+                RaiseNotifyPropertyChanged("Database");
+            }
+        }
+        public string Username {
+            get => username;
+            set {
+                username = value;
+                RaiseNotifyPropertyChanged("Username");
+            }
+        }
+        public string Password {
+            private get => password;
+            set {
+                password = value;
+                RaiseNotifyPropertyChanged("Password");
+            }
+        }
+        public ISqlAuthenticator Authenticator {
+            get => authenticator;
+            set {
+                authenticator = value;
+                authenticator.LoadDefaults();
+                Username = authenticator.Username;
+                Password = authenticator.Password;
+                CanEditCredentials = !authenticator.IsReadonly;
+                RaiseNotifyPropertyChanged("Authenticator");
+            }
+        }
+
+        public bool IsValid {
+            get;
+            set;
+        }
+        public bool CanEditCredentials {
+            get => canEditCredentials;
+            set {
+                canEditCredentials = value;
+                RaiseNotifyPropertyChanged("CanEditCredentials");
+            }
+        }
+
+        public List<string> Hostnames {
+            get => hostnames;
+            set {
+                hostnames = value;
+                RaiseNotifyPropertyChanged("Hostnames");
+            }
+        }
+        public List<string> Databases {
+            get => databases;
+            set {
+                databases = value;
+                RaiseNotifyPropertyChanged("Hostnames");
+            }
+        }
+        public ObservableCollection<ISqlAuthenticator> Authenticators {
+            get => authenticators;
+            set {
+                authenticators = value;
+                RaiseNotifyPropertyChanged("Authenticators");
+            }
+        }
+
+        public ICommand TestCommand;
+        public ICommand SaveCommand;
+        public ICommand LoadedCommand;
+
+        public ConnectionViewModel()
+        {
+            TestCommand = new RelayCommand((args) => OnTest());
+            SaveCommand = new RelayCommand((args) => OnSave());
+            LoadedCommand = new RelayCommand(async (args) => await OnLoaded());
+            
+        }
+
+        public async Task OnLoaded()
+        {
+        }
+
+        private void OnTest() { }
+        private void OnSave() { }
+        private void RaiseNotifyPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
